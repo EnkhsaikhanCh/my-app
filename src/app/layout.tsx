a673bbcde2e7b821c051d6ca0ca69c38226d0aa1
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-10-17 19:49:40 +0800

    Add Prettier Tailwind plugin and integrate Nuqs
    Added .prettierrc with Prettier Tailwind plugin configuration and updated dependencies in package.json to include nuqs, zustand, prettier, and prettier-plugin-tailwindcss. Wrapped app children with NuqsAdapter in layout.tsx to enable Nuqs integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -35,7 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <NuqsAdapter>{children}</NuqsAdapter>
           <Toaster richColors position={"bottom-right"} />
         </ThemeProvider>
       </body>
