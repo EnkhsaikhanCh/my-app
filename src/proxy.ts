2d8e4c50241034d21f2eff6d24ae3c99aac077dc
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-10-23 12:42:09 +0800

    Next.js 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 import { auth } from "./lib/auth";
 
-export const runtime = "nodejs";
-
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // 1. Get session
