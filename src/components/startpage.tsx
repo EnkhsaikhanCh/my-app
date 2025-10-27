c50725d0062bd595ae06182a41dc35a77870e256
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-10-28 00:08:52 +0800

    update

--- src/components/startpage.tsx
+++ src/components/startpage.tsx
@@ -1,9 +1,13 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
 import { Hero } from "./hero";
 
 export function StartPage() {
   return (
-    <>
+    <div className="bg-background flex min-h-screen flex-col">
+      <Header />
       <Hero />
-    </>
+      <Footer />
+    </div>
   );
 }
